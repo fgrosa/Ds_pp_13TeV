1e15e70d31153a2937ea955d9adbf47e4e1198bf
fchinu <fachi2001@gmail.com>
2025-02-28 13:27:50 +0100

    make variables names more clear

--- FD_Fraction/data_driven/VsMult/create_config_files_proj.py
+++ FD_Fraction/data_driven/VsMult/create_config_files_proj.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     proj_config_file = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/config_projection_data_FT0_2023.yaml"
     cutset_dir = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/configs/cutsets"
     fit_config_file = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/config_fit_data_FT0_multiplicity_23_doublecb.yml"
-    proj_out_dir = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/Projections_RawYields/doublecb"
-    proj_in_dir = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/Projections_RawYields/doublecb" # different from proj_out_dir since we merge the projections
+    proj_out_dir = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/Projections_RawYields"
+    fit_in_dir = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/Projections_RawYields" # different from proj_out_dir since we merge the projections
+    fit_out_dir = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/Projections_RawYields/doublecb" # different from proj_out_dir since we merge the projections
     eff_config_file = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/config_efficiency_LHC24h1.yaml"
     eff_out_dir = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/Efficiency"
     output_dir = "/home/fchinu/Run3/Ds_pp_13TeV/FD_Fraction/data_driven/VsMult/mult_differential/2023/configs"
@@ -32,10 +33,11 @@ if __name__ == "__main__":
             yaml.dump(proj_config_mod, file, default_flow_style=False)
 
         fit_config_mod = copy.deepcopy(fit_config)
-        data_proj_file = os.path.join(proj_in_dir, f"projection_data{suffix}.root")
-        fit_config_mod["inputs"]["data"] = data_proj_file
+        data_proj_file = os.path.join(fit_in_dir, f"projection_data{suffix}.root")
+        data_proj_file = os.path.join(fit_in_dir, f"projection_data{suffix}.root")
+        fit_config_mod["inputs"]["data"]= data_proj_file
         fit_config_mod["inputs"]["cutset"] = cutset_file_path
-        fit_config_mod["outputs"]["directory"] = proj_out_dir
+        fit_config_mod["outputs"]["directory"] = fit_out_dir
         fit_config_mod["outputs"]["save_all_fits"] = True
         fit_config_mod["outputs"]["suffix"] = suffix
         out_file_name = f"config_fit{suffix}.yaml"
@@ -53,10 +55,3 @@ if __name__ == "__main__":
         out_file_path = os.path.join(output_dir, "efficiencies", out_file_name)
         with open(out_file_path, "w", encoding="utf-8") as file:
             yaml.dump(eff_config_mod, file, default_flow_style=False)
-
-
-
-
-        
-
-    
